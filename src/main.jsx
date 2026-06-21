81a60a419688af5b47ec61b419231ce4a1f2f060
Shehrozbasra <161588740+Shehrozbasra@users.noreply.github.com>
2026-06-21 04:40:04 +0100

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import StoreApp from "./StoreApp.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <StoreApp />
-  </React.StrictMode>
-);
